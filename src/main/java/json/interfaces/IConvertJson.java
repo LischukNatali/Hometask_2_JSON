e20016a08007fcf458b32ec65387651ba4951caa
LischukNatali <natalia4878@gmail.com.ua>
2017-08-01 15:11:04 +0300

    .

--- src/main/java/json/interfaces/IConvertJson.java
+++ src/main/java/json/interfaces/IConvertJson.java
@@ -2,13 +2,11 @@ package json.interfaces;
 
 public interface IConvertJson {
 
-//      может принимать любой объект и возвращать его JSON-представление
-//      {"firstName": "Ivan","lastName": "Ivanov","fun": "Guitar","birthDate": "01-09-1990" }
+//      Метод принимает любой объект и возвращать его JSON-представление
+//      {"firstName":"Ivan","lastName":"Ivanov","fun":"Guitar","birthDate":"01-09-1990"}
     public String toJson(Object o) throws IllegalAccessException;
 
-//    Реализовать механизм вычитки данных из строки JSON’а. Реализовать метод fromJson(String json, Class<T> clazz),
-//    где json - входящая JSON строка, а clazz - класс в который нужно распарсить входящие данные.
-//    String fromJson = "{\"firstName\":\"fName\",\"lastName\":\"lName\",\"hobby\":\"hb\", \"birthDate\":\"01012011\"}";
+//    Метод считывает данные из строки JSON’а.
     public Object fromJson(String json, Class clazz) throws IllegalAccessException, InstantiationException;
 }
 
